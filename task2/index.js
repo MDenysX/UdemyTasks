f04ddf0d3a313627a35c0bed3708832e477f2996
MDenysX <zdosans@gmail.com>
2025-06-12 22:29:14 +0300

    added task2

--- task2/index.js
+++ task2/index.js
@@ -1,11 +1,10 @@
-function test() {
-    var a = "3";
-    var b = "8";
+function lifeInWeeks(age) {
 
-    var temp = a;
-    a = b;
-    b = temp;
+    var yearsRemaining = 90 - age;
+    var days = yearsRemaining * 365;
+    var weeks = yearsRemaining * 52;
+    var months = yearsRemaining * 12;
+    
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
 
-    console.log("a is " + a);
-    console.log("b is " + b);
 }
